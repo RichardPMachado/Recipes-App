b067a788614664eb9837336a68185084b13fe4bc
RichardPMachado <richardmachado2211@gmail.com>
2022-10-24 23:53:22 -0300

    organizando Rotas e Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import Drinks from './components/Drinks';
-import Login from './components/Login';
-import Meals from './components/Meals';
+import Drinks from './Pages/Drinks';
+import Login from './Pages/Login';
+import Meals from './Pages/Meals';
 import AppProvider from './Context/AppProvider';
+import Profile from './Pages/Profile';
+import Done from './Pages/Done';
+import Favorite from './Pages/Favorite';
 
 function App() {
   return (
@@ -13,6 +16,9 @@ function App() {
           <Route exact path="/" component={ Login } />
           <Route exact path="/meals" component={ Meals } />
           <Route exact path="/drinks" component={ Drinks } />
+          <Route exact path="/profile" component={ Profile } />
+          <Route exact path="/done-recipes" component={ Done } />
+          <Route exact path="/favorite-recipes" component={ Favorite } />
         </Switch>
       </BrowserRouter>
     </AppProvider>
---
+++ src/Pages/Done.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import Header from '../components/Header';
+
+export default function Done() {
+  return (
+    <div>
+      <Header titulo="Done Recipes" />
+    </div>
+  );
+}
